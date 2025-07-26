b8a3eedc1cb788f7cf592c6690660cb7a91f522d
He Ronghua <heronghua1989@126.com>
2025-07-26 18:07:14 +0800

    Fix native host messaging not works in web-extensions

--- web_extensions/open-path-explorer/native-host-setup/install_host.bat
+++ web_extensions/open-path-explorer/native-host-setup/install_host.bat
@@ -1,13 +1,13 @@
 @echo off
-setlocal
+setlocal enabledelayedexpansion
 
-:: 设置变量
+:: set environment
 set HOST_NAME=com.example.open_path_host
 set HOST_PATH=%cd%\open_path_host.py
 set MANIFEST_PATH=%APPDATA%\Mozilla\NativeMessagingHosts\%HOST_NAME%.json
 set CHROME_MANIFEST_PATH=%LOCALAPPDATA%\Google\Chrome\User Data\NativeMessagingHosts\%HOST_NAME%.json
 
-:: 创建Firefox的manifest文件
+:: TODO test create Firefox manifest
 echo {
 echo   "name": "%HOST_NAME%",
 echo   "description": "Open Path Host",
@@ -17,16 +17,24 @@ echo   "args": ["%HOST_PATH%"],
 echo   "allowed_extensions": ["open-path-with-explorer@example.com"]
 echo } > "%MANIFEST_PATH%"
 
-:: 创建Chrome的manifest文件
-echo {
-echo   "name": "%HOST_NAME%",
-echo   "description": "Open Path Host",
-echo   "path": "python",
-echo   "type": "stdio",
-echo   "allowed_origins": [
-echo       "chrome-extension://你的扩展ID/"
-echo   ]
-echo } > "%CHROME_MANIFEST_PATH%"
+:: create Chrome manifest
+:: create bat
+echo @echo off > %cd%\open_path_host.bat
+echo python %cd%\open_path_host.py >> %cd%/open_path_host.bat
 
-echo 安装完成，请确保已安装Python。
+echo { > "%CHROME_MANIFEST_PATH%"
+echo   "name": "%HOST_NAME%", >> "%CHROME_MANIFEST_PATH%"
+echo   "description": "Open Path Host", >> "%CHROME_MANIFEST_PATH%" 
+
+set "currentDir=%cd%"
+set "currentDir=!currentDir:\=\\!"
+echo   "path": "!currentDir!\\open_path_host.bat", >> "%CHROME_MANIFEST_PATH%"
+
+echo   "type": "stdio", >> "%CHROME_MANIFEST_PATH%"
+echo   "allowed_origins": [ >> "%CHROME_MANIFEST_PATH%"
+echo       "chrome-extension://你的扩展ID/" >> "%CHROME_MANIFEST_PATH%"
+echo   ] >> "%CHROME_MANIFEST_PATH%"
+echo } >> "%CHROME_MANIFEST_PATH%"
+
+echo install complete , ensure you have python environment.
 endlocal
